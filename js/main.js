4027c04f97d61066acd779de97d2b70f4486722a
yancymin <senseslofter@gmail.com>
2018-05-30 13:52:46 +0800

    头像hover

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-var changebtn = $("changebtn")
+
+
+var hoverbox = document.getElementById('hoverbox');
+var pic = document.getElementById('pic');
+var change = document.getElementById('change');
+
+// pic.addEventListener("mouseenter", function () {
+//     this.style.display="none";
+//     hoverbox.style.display="block";
+
+    
+// });
+
+// pic.addEventListener("mouseleave", function () {
+//     this.style.display="block";
+//     hoverbox.style.display="none";
+// });
+
+
+change.addEventListener("mouseenter", function () {
+    pic.style.display="none";
+    hoverbox.style.display="block";
+
+    
+});
+
+change.addEventListener("mouseleave", function () {
+    pic.style.display="block";
+    hoverbox.style.display="none";
+});
